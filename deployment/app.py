550276a0cb5e0dc98188f76ffc1b91f4106c5916
Stuti Sharma <52918756+StutiSharma22@users.noreply.github.com>
2026-09-20 16:35:08 +0530

    Add files via upload

---
+++ deployment/app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("model.joblib")
+
+@app.route("/")
+def home():
+    return "Engine Condition Prediction API is running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.get_json()
+
+    input_df = pd.DataFrame([data])
+
+    prediction = model.predict(input_df)
+
+    return jsonify({
+        "prediction": int(prediction[0])
+    })
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=7860
+    )
